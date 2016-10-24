e63cac51e0529d5c82d6921d1bf394465430d507
fawin <falco.winkler@esailors.de>
2016-10-24 22:35:09 +0200

    renamed hello package to commuml

--- src/main/java/commuml/MainController.java
+++ src/main/java/commuml/MainController.java
@@ -1,9 +1,8 @@
-package hello;
+package commuml;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
