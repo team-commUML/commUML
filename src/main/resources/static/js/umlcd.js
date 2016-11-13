48ed77e112e984719d17da0bd8966aa602181f8d
lukas-haw <lukasreinkemeier@gmx.de>
2016-11-13 15:53:09 +0100

    Add files via upload

--- src/main/resources/static/js/umlcd.js
+++ src/main/resources/static/js/umlcd.js
@@ -181,6 +181,133 @@ _.each(relations, function (r) {
     graph.addCell(r);
 });
 
+
+//Creation of Property-Panel
+//Custom Element:
+(function() {
+joint.shapes.html = {};
+    joint.shapes.html.Element = joint.shapes.basic.Rect.extend({
+        defaults: joint.util.deepSupplement({
+            type: 'html.Element',
+            attrs: {
+                rect: { stroke: 'none', 'fill-opacity': 0 }
+            }
+        }, joint.shapes.basic.Rect.prototype.defaults)
+    });
+
+//Custom View
+joint.shapes.html.ElementView = joint.dia.ElementView.extend({
+
+        template: [
+            '<div class="html-element">',
+            //'<button class="delete">x</button>',
+            '<label></label>',
+
+            // '<span></span>', '<br/>',
+            //'<select><option>--</option><option>one</option><option>two</option></select>',
+
+            '<textarea id="Klassenname"></textarea>',
+            '<textarea id="Attribute"></textarea>',
+            '<textarea id="Methoden"></textarea>',
+            '</div>'
+        ].join(''),
+
+        initialize: function() {
+            _.bindAll(this, 'updateBox');
+            joint.dia.ElementView.prototype.initialize.apply(this, arguments);
+
+            this.$box = $(_.template(this.template)());
+            // Prevent paper from handling pointerdown.
+            this.$box.find('textarea,select').on('mousedown click', function(evt) {
+                evt.stopPropagation();
+            });
+            // This is an example of reacting on the input change and storing the input data in the cell model.
+            this.$box.find('#Klassenname').on('change', _.bind(function(evt) {
+                this.model.set('neuKlassenName', $(evt.target).val());
+                graph.getCell(this.model.get('bezugsklasse')).set('name',this.model.get('neuKlassenName'));
+            }, this));
+            this.$box.find('#Attribute').on('change', _.bind(function(evt) {
+                            this.model.set('neuAttribute', $(evt.target).val());
+                            graph.getCell(this.model.get('bezugsklasse')).set('attributes',this.model.get('neuAttribute'));
+                        }, this));
+            this.$box.find('#Methoden').on('change', _.bind(function(evt) {
+                             this.model.set('neuMethoden', $(evt.target).val());
+                             graph.getCell(this.model.get('bezugsklasse')).set('methods',this.model.get('neuMethoden'));
+                         }, this));
+
+//            this.$box.find('select').on('change', _.bind(function(evt) {
+//                this.model.set('select', $(evt.target).val());
+//            }, this));
+//            this.$box.find('select').val(this.model.get('select'));
+
+            this.$box.find('.delete').on('click', _.bind(this.model.remove, this.model));
+            // Update the box position whenever the underlying model changes.
+            this.model.on('change', this.updateBox, this);
+            // Remove the box when the model gets removed from the graph.
+            this.model.on('remove', this.removeBox, this);
+
+            this.updateBox();
+        },
+        render: function() {
+            joint.dia.ElementView.prototype.render.apply(this, arguments);
+            this.paper.$el.prepend(this.$box);
+            this.updateBox();
+            return this;
+        },
+        updateBox: function() {
+            // Set the position and dimension of the box so that it covers the JointJS element.
+            var bbox = this.model.getBBox();
+            // Example of updating the HTML with a data stored in the cell model.
+            this.$box.find('label').text(this.model.get('label'));
+            this.$box.find('span').text(this.model.get('select'));
+            if (!(this.model.get('bezugsklasse')==='undefined')) {
+
+                this.model.set('klassenName',graph.getCell(this.model.get('bezugsklasse')).get('name'));
+                this.model.set('attribute',graph.getCell(this.model.get('bezugsklasse')).get('attributes'));
+                this.model.set('methoden',graph.getCell(this.model.get('bezugsklasse')).get('methods'));
+            }
+            this.$box.find('#Klassenname').text(this.model.get('klassenName'));
+            this.$box.find('#Attribute').text(this.model.get('attribute'));
+            this.$box.find('#Methoden').text(this.model.get('methoden'));
+
+
+
+            this.$box.css({
+                width: bbox.width,
+                height: bbox.height,
+                left: bbox.x,
+                top: bbox.y,
+                transform: 'rotate(' + (this.model.get('angle') || 0) + 'deg)'
+            });
+        },
+        removeBox: function(evt) {
+            this.$box.remove();
+        }
+
+
+    });
+
+
+}())
+
+var propertyBox = new joint.shapes.html.Element({
+        position: { x: 80, y: 80 },
+        size: { width: 170, height: 100 },
+        label: 'Property Box',
+        klassenName: 'Klassenname',
+        attribute: 'Attribute',
+        methoden: 'Methoden',
+        bezugsklasse: 'undefined',
+        neuKlassenname: 'neuKlassenname',
+        neuAttribute: 'neuAttribute',
+        neuMethoden: 'neuMethoden'
+        //select: 'one'
+    });
+
+
+
+graph.addCells([propertyBox]);
+
 function addClassDiagram() {
     graph.addCell(new uml.Class({
         position: {x: 100, y: 100},
@@ -222,23 +349,36 @@ paper.on('cell:pointerdown',
             }
         }
 
+        propertyBox.set('bezugsklasse',cellView.model.id);
+        //propertyBox.set('klassenName',cellView.model.get('name'));
+        paper.findViewByModel(propertyBox).updateBox();
+
+
+
+
+
     }
 );
 
 
 function addInheritance() {
+    clicks = [];
     relationClass = uml.Generalization;
 }
 function addDirectedAssociation() {
+    clicks = [];
     relationClass = uml.Transition;
 }
 function addUndirectedAssociation() {
+    clicks = [];
     relationClass = uml.Association;
 }
 function addAggregation() {
+    clicks = [];
     relationClass = uml.Aggregation;
 }
 function addComposition() {
+    clicks = [];
     relationClass = uml.Composition;
 }
 
